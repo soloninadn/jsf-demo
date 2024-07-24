49ba0c65a75e418d018596baff4e6c37c2f8dc28
dsolonina <dsolonina@gmail.com>
2024-07-24 15:29:58 +0300

    SQL injection

--- src/main/java/com/example/jsfspringboot/service/EmployeeService.java
+++ src/main/java/com/example/jsfspringboot/service/EmployeeService.java
@@ -1,12 +1,17 @@
 package com.example.jsfspringboot.service;
 
+import com.example.jsfspringboot.model.bean.EmployeeBean;
 import com.example.jsfspringboot.model.entity.EmployeeEntity;
 import com.example.jsfspringboot.repository.EmployeeRepository;
+import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import com.example.jsfspringboot.model.bean.EmployeeBean;
 
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,9 +21,11 @@ public class EmployeeService {
     private final Logger LOGGER = LoggerFactory.getLogger(EmployeeService.class);
 
     private final EmployeeRepository employeeRepository;
+    private final DataSource dataSource;
 
-    public EmployeeService(EmployeeRepository employeeRepository) {
+    public EmployeeService(EmployeeRepository employeeRepository, DataSource dataSource) {
         this.employeeRepository = employeeRepository;
+        this.dataSource = dataSource;
     }
 
     public List<EmployeeBean> getAllEmployees() {
@@ -26,6 +33,23 @@ public class EmployeeService {
         List<EmployeeBean> employeeBeanList = mapEntityListToBeanList(employeeRepository.findAll());
         LOGGER.info("getAllEmployees end, found: {} employees", employeeBeanList.size());
         return employeeBeanList;
+    }
+
+    public List<EmployeeBean> getEmployee(String id) {
+        LOGGER.info("getEmployee started: id= "+id);
+
+        try {
+            Statement st = dataSource.getConnection().createStatement();
+            ResultSet resultSet = st.executeQuery("SELECT name FROM user_data WHERE first_name = 'John' and last_name = '"+id+"'");
+            String r = resultSet.getString(1);
+            EmployeeBean e = new EmployeeBean();
+            e.setName(r);
+            return Lists.newArrayList(e);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     public EmployeeBean save(EmployeeBean employeeBean) {
